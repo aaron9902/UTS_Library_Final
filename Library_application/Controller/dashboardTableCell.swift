1ba051a54d406d4af409797a3f82e8925c7b0f37
NilofarParveen <mariamnilo8896@gmail.com>
2021-05-15 17:17:31 +0530

    UI Changes
    Commiting
    Dashboard UI change
    Checkout UI Change

---
+++ Library_application/Controller/dashboardTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  dashboardTableCell.swift
+//  Library_application
+//
+//  Created by user190351 on 5/15/21.
+//
+
+import UIKit
+
+class dashboardTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
